40f12e2f7ea7c1ca118b75e7ddf742f3828cff88
onesmus odongo <odongo1999@gmail.com>
2024-09-15 23:38:44 +0300

    update solution

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,5 +1,23 @@
 class Author:
-    pass
+
+    all = []
+
+    def __init__(self, name):
+        self.name = name
+        Author.all.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+
+    def books(self):
+        return [contract.book for contract in self.contracts()]
+
+    def sign_contract(self, book, date, royalties):
+        return Contract(self, book, date, royalties)
+    
+    def total_royalties(self):
+        return sum([contract.royalties for contract in self.contracts()])
+
 
 
 class Book:
